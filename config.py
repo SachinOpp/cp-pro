5cbcca0f5dc41ca0d035e56f65bb45defeb41246
ItsSanatani <sachinxmeena2000@gmail.com>
2025-04-09 17:19:21 +0530

    ❖ | sᴧᴄʜɪɴ ꭙ sᴧɴᴧᴛᴧɴɪ | ❖

--- config.py
+++ config.py
@@ -1,23 +1,28 @@
-
-# --------------M----------------------------------
-
 import os
 from os import getenv
-# ---------------R---------------------------------
-API_ID = int(os.environ.get("API_ID"))
-# ------------------------------------------------
-API_HASH = os.environ.get("API_HASH")
-# ----------------D--------------------------------
-BOT_TOKEN = os.environ.get("BOT_TOKEN")
-# -----------------A-------------------------------
-BOT_USERNAME = os.environ.get("BOT_USERNAME")
-# ------------------X------------------------------
-OWNER_ID = int(os.environ.get("OWNER_ID"))
-# ------------------X------------------------------
-# ------------------------------------------------
-LOGGER_ID = int(os.environ.get("LOGGER_ID"))
-# ------------------------------------------------
-MONGO_URL = os.environ.get("MONGO_URL")
+
 # ------------------------------------------------
 
+API_ID = int(os.environ.get("API_ID", "28795512"))
+API_HASH = os.environ.get("API_HASH", "c17e4eb6d994c9892b8a8b6bfea4042a")
 
+# ------------------------------------------------
+
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "7239027308:AAEBqtklQD_K-jYkahMI-hUaMiEOvR3Tw40")
+BOT_USERNAME = os.environ.get("BOT_USERNAME", "TheSafeRobot")
+
+# -----------------------------------------------
+
+OWNER_ID = int(os.environ.get("OWNER_ID", "7879180190"))
+
+# ------------------------------------------------
+
+# ------------------------------------------------
+LOGGER_ID = int(os.environ.get("LOGGER_ID", "-1002664280831"))
+OTHER_LOGS = int(os.environ.get("OTHER_LOGS", "-1002644496954"))
+
+# ------------------------------------------------
+
+MONGO_URL = os.environ.get("MONGO_URL", "mongodb+srv://Radheee:sanatani@cluster0.sgop4.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+
+# ------------------------------------------------
