f4a24bdbb45cfd8a22bfc935b128fa7a7aa1c765
ItsSanatani <sachinxmeena2000@gmail.com>
2025-04-09 21:36:47 +0530

    ❖ | sᴧᴄʜɪɴ ꭙ sᴧɴᴧᴛᴧɴɪ | ❖

--- MAFU/helper/sudo.py
+++ MAFU/helper/sudo.py
@@ -1,4 +1,4 @@
-from . import sudodb
+"""from . import sudodb
 
 async def add_sudo(user_id: int):
     """
@@ -27,3 +27,41 @@ async def is_sudo(user_id: int) -> bool:
     Check if a user is in sudo list.
     """
     return bool(await sudodb.find_one({"user_id": user_id}))
+    """
+#=×=×=×=×=×=×=×=×=×=×=×=×=×=×=×=×=×=×=×=×=×=×=×=×=×=×=×=×=×=×=×=×
+
+import datetime
+from config import MONGO_URL
+from motor.motor_asyncio import AsyncIOMotorClient as MongoCli
+
+mongo = MongoCli(MONGO_URL)
+sudodb = mongo.sudo.sudoers  # You can change this collection name as you prefer
+
+# SUDO FUNCTIONS
+
+async def get_sudoers():
+    sudo_list = []
+    async for user in sudodb.find({}):
+        sudo_list.append(user["user_id"])
+    return {"sudoers": sudo_list}
+
+async def is_sudo(user_id: int):
+    data = await get_sudoers()
+    return user_id in data["sudoers"]
+
+async def add_sudo(user_id: int):
+    if await is_sudo(user_id):
+        return
+    await sudodb.insert_one({
+        "user_id": user_id,
+        "added_at": datetime.datetime.utcnow()
+    })
+
+async def remove_sudo(user_id: int):
+    if not await is_sudo(user_id):
+        return
+    await sudodb.delete_one({"user_id": user_id})
+
+async def get_new_sudoers():
+    one_day_ago = datetime.datetime.utcnow() - datetime.timedelta(days=1)
+    return await sudodb.count_documents({"added_at": {"$gte": one_day_ago}})
