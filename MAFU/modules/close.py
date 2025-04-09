90396f6ec4b27bdc1e1e32b0e43ddb0c88abf125
ItsSanatani <sachinxmeena2000@gmail.com>
2025-04-09 21:57:22 +0530

    ❖ | sᴧᴄʜɪɴ ꭙ sᴧɴᴧᴛᴧɴɪ | ❖

--- MAFU/modules/close.py
+++ MAFU/modules/close.py
@@ -1,20 +1,25 @@
-import asyncio
 from pyrogram import Client, filters
-from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery
+from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery
+import asyncio
 
-close_markup = InlineKeyboardMarkup(
-    [[InlineKeyboardButton("✖️ Close", callback_data="close")]]
-)
+@app.on_message(filters.command("test"))
+async def test_cmd(_, message: Message):
+    await message.reply_text(
+        "ᴀᴇ ʙᴀʙᴜ, ʏᴇ ᴛᴇꜱᴛ ʜᴀɪ...",
+        reply_markup=InlineKeyboardMarkup(
+            [[InlineKeyboardButton("ᴄʟᴏsᴇ", callback_data="close")]]
+        )
+    )
 
-@Client.on_callback_query(filters.regex("close"))
+@app.on_callback_query(filters.regex("close"))
 async def close_menu(_, query: CallbackQuery):
     try:
         await query.answer()
         await query.message.delete()
-        umm = await query.message.reply_text(
+        closed = await query.message.reply_text(
             f"ᴄʟᴏꜱᴇᴅ ʙʏ : {query.from_user.mention}"
         )
         await asyncio.sleep(2)
-        await umm.delete()
+        await closed.delete()
     except:
         pass
