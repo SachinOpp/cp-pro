f2926d099dd772352ea7f54f676af844235a7808
ItsSanatani <sachinxmeena2000@gmail.com>
2025-04-09 17:53:25 +0530

    ❖ | sᴧᴄʜɪɴ ꭙ sᴧɴᴧᴛᴧɴɪ | ❖

--- MAFU/modules/start.py
+++ MAFU/modules/start.py
@@ -1,29 +1,33 @@
 from pyrogram import filters
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message, CallbackQuery
-
 from config import OWNER_ID, BOT_USERNAME
 import config
 from MAFU import MAFU as app
-
 
 START_IMG = "https://files.catbox.moe/jhlnjc.jpg"
 START_CAPTION = "**✨ ʜᴇʏ ʙᴀʙʏ! ɪ'ᴍ ᴀʟɪᴠᴇ ᴀɴᴅ ʀᴇᴀᴅʏ ᴛᴏ ᴘʀᴏᴛᴇᴄᴛ ʏᴏᴜʀ ɢʀᴏᴜᴘ ✨**"
 
 START_BUTTONS = InlineKeyboardMarkup([
-    [
-        InlineKeyboardButton("• ᴀᴅᴅ ᴍᴇ ɪɴ ʏᴏᴜʀ ɢʀᴏᴜᴘ •", url=f"https://t.me/{BOT_USERNAME}?startgroup=true")],
+    [InlineKeyboardButton("• ᴀᴅᴅ ᴍᴇ ɪɴ ʏᴏᴜʀ ɢʀᴏᴜᴘ •", url=f"https://t.me/{BOT_USERNAME}?startgroup=true")],
     [
         InlineKeyboardButton("• ʜᴇʟᴘ •", callback_data="show_help"),
         InlineKeyboardButton("• ᴀʙᴏᴜᴛ •", callback_data="show_about")
     ],
-    [
-        InlineKeyboardButton("• ᴏᴡɴᴇʀ •", user_id=config.OWNER_ID)
-    ]
+    [InlineKeyboardButton("• ᴏᴡɴᴇʀ •", user_id=config.OWNER_ID)]
 ])
 
 
-@app.on_message(filters.command("start") & filters.private)
+# /start command
+@app.on_message(filters.command("start"))
 async def start_command(client, message: Message):
+    if message.chat.type != "private":
+        return await message.reply(
+            "**ᴜsᴇ ᴛʜɪs ᴄᴏᴍᴍᴀɴᴅ ɪɴ ᴍʏ ᴘʀɪᴠᴀᴛᴇ ᴄʜᴀᴛ.**",
+            reply_markup=InlineKeyboardMarkup([
+                [InlineKeyboardButton("• ᴄʟɪᴄᴋ ʜᴇʀᴇ •", url=f"https://t.me/{BOT_USERNAME}?start=start")]
+            ])
+        )
+
     await message.reply_photo(
         photo=START_IMG,
         caption=START_CAPTION,
@@ -31,10 +35,35 @@ async def start_command(client, message: Message):
     )
 
 
+# /help command
+@app.on_message(filters.command("help"))
+async def help_command(client, message: Message):
+    if message.chat.type != "private":
+        return await message.reply(
+            "**ʜᴇʟᴘ ᴄᴏᴍᴍᴀɴᴅ ɪs ᴏɴʟʏ ᴀᴠᴀɪʟᴀʙʟᴇ ɪɴ ᴅᴍ.**",
+            reply_markup=InlineKeyboardMarkup([
+                [InlineKeyboardButton("• ᴏᴘᴇɴ ᴅᴍ •", url=f"https://t.me/{BOT_USERNAME}?start=help")]
+            ])
+        )
+
+    await message.reply_text(
+        "**❖ ʜᴇʟᴘ ᴍᴇɴᴜ ⏤͟͟͞͞★**\n\n"
+        "**● /start ➥ sᴛᴀʀᴛ ʙᴏᴛ\n"
+        "● /ping ➥ ᴄʜᴇᴄᴋ ʙᴏᴛ ᴘɪɴɢ\n"
+        "● /repo ➥ ʀᴇᴘᴏ ʟɪɴᴋ**",
+        reply_markup=InlineKeyboardMarkup([
+            [InlineKeyboardButton("• ʙᴀᴄᴋ •", callback_data="go_back")]
+        ])
+    )
+
+
 @app.on_callback_query(filters.regex("show_help"))
 async def show_help(_, query: CallbackQuery):
     await query.message.edit_caption(
-        "**❖ ʜᴇʟᴘ ᴍᴇɴᴜ ⏤͟͟͞͞★\n\n● /start ➥ sᴛᴀʀᴛ ʙᴏᴛ\n● /ping ➥ ᴄʜᴇᴄᴋ ʙᴏᴛ ᴘɪɴɢ\n● /repo ➥ ʀᴇᴘᴏ ʟɪɴᴋ**",
+        "**❖ ʜᴇʟᴘ ᴍᴇɴᴜ ⏤͟͟͞͞★\n\n"
+        "● /start ➥ sᴛᴀʀᴛ ʙᴏᴛ\n"
+        "● /ping ➥ ᴄʜᴇᴄᴋ ʙᴏᴛ ᴘɪɴɢ\n"
+        "● /repo ➥ ʀᴇᴘᴏ ʟɪɴᴋ**",
         reply_markup=InlineKeyboardMarkup([
             [InlineKeyboardButton("• ʙᴀᴄᴋ •", callback_data="go_back")]
         ])
