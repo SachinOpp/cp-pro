1e501cfdb12baa80c0062e917942e50ae565a2b1
ItsSanatani <sachinxmeena2000@gmail.com>
2025-04-09 22:00:28 +0530

    ❖ | sᴧᴄʜɪɴ ꭙ sᴧɴᴧᴛᴧɴɪ | ❖

--- MAFU/modules/close.py
+++ MAFU/modules/close.py
@@ -3,22 +3,13 @@ from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton, 
 import asyncio
 from MAFU import MAFU as app
 
-@app.on_message(filters.command("test"))
-async def test_cmd(_, message: Message):
-    await message.reply_text(
-        "ᴀᴇ ʙᴀʙᴜ, ʏᴇ ᴛᴇꜱᴛ ʜᴀɪ...",
-        reply_markup=InlineKeyboardMarkup(
-            [[InlineKeyboardButton("ᴄʟᴏsᴇ", callback_data="close")]]
-        )
-    )
-
 @app.on_callback_query(filters.regex("close"))
 async def close_menu(_, query: CallbackQuery):
     try:
         await query.answer()
         await query.message.delete()
         closed = await query.message.reply_text(
-            f"ᴄʟᴏꜱᴇᴅ ʙʏ : {query.from_user.mention}"
+            f"✅ Closed by : {query.from_user.mention}"
         )
         await asyncio.sleep(2)
         await closed.delete()
