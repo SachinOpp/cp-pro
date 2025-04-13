a44c1b69b2c1803d46d15746b08b5e268e27822d
ItsSanatani <sachinxmeena2000@gmail.com>
2025-04-13 22:06:54 +0530

    ❖ | sᴧᴄʜɪɴ ꭙ sᴧɴᴧᴛᴧɴɪ | ❖

--- MAFU/modules/start.py
+++ MAFU/modules/start.py
@@ -29,7 +29,8 @@ START_BUTTONS = InlineKeyboardMarkup([
         InlineKeyboardButton("• ᴜᴘᴅᴀᴛᴇ •", url="https://t.me/Copyright_Community")
     ],
     [
-        InlineKeyboardButton("• ʜᴇʟᴘ ᴀɴᴅ ᴄᴏᴍᴍᴀɴᴅ •", callback_data="show_help")]
+        InlineKeyboardButton("• ʜᴇʟᴘ ᴀɴᴅ ᴄᴏᴍᴍᴀɴᴅ •", callback_data="show_help")
+    ]
 ])
 
 PRIVATE_START_BUTTON = InlineKeyboardMarkup([
@@ -40,47 +41,23 @@ PRIVATE_START_BUTTON = InlineKeyboardMarkup([
 
 @app.on_message(filters.command("start"))
 async def start_command(_, message: Message):
-    if message.chat.type.name == "PRIVATE":
-        await add_user(message.from_user.id)
-        caption = get_start_caption(message.from_user)
-        return await message.reply_photo(
+    user = message.from_user
+    chat = message.chat
+
+    # Add to database
+    await add_user(user.id)
+    if chat.type in ["group", "supergroup"]:
+        await add_chat(chat.id)
+
+    # If private chat
+    if message.chat.type == "private":
+        await message.reply_photo(
             photo=START_IMG,
-            caption=caption,
+            caption=get_start_caption(user),
             reply_markup=START_BUTTONS
         )
     else:
-        return await message.reply(
-            "**ᴘʟᴇᴀꜱᴇ ᴜꜱᴇ /start ɪɴ ᴘʀɪᴠᴀᴛᴇ ᴄʜᴀᴛ ꜰᴏʀ ᴍᴏʀᴇ ɪɴꜰᴏ.**",
+        await message.reply_text(
+            text=f"**ʜᴇʏ {user.mention}, ᴛʜᴀɴᴋꜱ ꜰᴏʀ ᴀᴅᴅɪɴɢ ᴍᴇ!**",
             reply_markup=PRIVATE_START_BUTTON
         )
-
-@app.on_message(filters.new_chat_members)
-async def group_welcome(_, message: Message):
-    for member in message.new_chat_members:
-        if member.id == (await app.get_me()).id:
-            await add_chat(message.chat.id)
-            try:
-                await add_user(message.from_user.id)
-            except:
-                pass
-            caption = f"""
-**ᴛʜᴀɴᴋꜱ {message.from_user.mention} ꜰᴏʀ ᴀᴅᴅɪɴɢ ᴍᴇ!**
-ɪ'ᴍ ɴᴏᴡ ᴀᴄᴛɪᴠᴇ ɪɴ ᴛʜɪꜱ ɢʀᴏᴜᴘ ᴀɴᴅ ʀᴇᴀᴅʏ ᴛᴏ ᴘʀᴏᴛᴇᴄᴛ.
-➥ ᴍᴀᴋᴇ ꜱᴜʀᴇ ᴛᴏ ᴄʜᴇᴄᴋ ᴍʏ ꜱᴇᴛᴛɪɴɢꜱ ᴀɴᴅ ɢɪᴠᴇ ᴘʀᴏᴘᴇʀ ʀɪɢʜᴛꜱ.
-"""
-            await message.reply_photo(
-                photo=START_IMG,
-                caption=caption,
-                reply_markup=START_BUTTONS
-            )
-
-@app.on_callback_query(filters.regex("go_back"))
-async def go_back(_, query):
-    caption = get_start_caption(query.from_user)
-    await query.message.edit_media(
-        media=InputMediaPhoto(
-            media=START_IMG,
-            caption=caption
-        ),
-        reply_markup=START_BUTTONS
-    )
